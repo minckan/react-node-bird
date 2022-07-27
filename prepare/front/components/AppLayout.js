344c9ea6043a1573b9acd1838557f596ad3467b3
mjkang <mjkang@12cm.co.kr>
2022-07-27 13:14:36 +0900

    Add: _app.js, LoginForm 추가

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import PropTypes from 'proptypes'
+import React, {useState} from 'react';
+import PropTypes from 'prop-types'
 import Link from 'next/link'
-import {Menu} from 'antd'
-import MenuItem from 'antd/lib/menu/MenuItem';
+import {Input, Menu, Row, Col} from 'antd'
+import LoginForm from './LoginForm';
+import UserProfile from './UserProfile';
+
 
 const AppLayout = ({children}) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
   return (
-    <Menu mode='horizontal'>
-      <MenuItem><Link href='/'><a>노드버드</a></Link></MenuItem>
-      <MenuItem><Link href='/profile'><a>프로필</a></Link></MenuItem>
-      <MenuItem><Link href='/signup'><a>회원가입</a></Link></MenuItem>
-      {children}
-    </Menu>
+    <>
+      <Menu mode='horizontal'>
+        <Menu.Item><Link href='/'><a>노드버드</a></Link></Menu.Item>
+        <Menu.Item><Link href='/profile'><a>프로필</a></Link></Menu.Item>
+        <Menu.Item><Input.Search enterButton></Input.Search></Menu.Item>
+        <Menu.Item><Link href='/signup'><a>회원가입</a></Link></Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile/> : <LoginForm/>}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href='' target='_blank' rel='noreferrer noopener'>Made by Minju</a>
+        </Col>
+      </Row>
+    </>
   )
 }
 
